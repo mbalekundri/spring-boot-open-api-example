41ec3f20429074636f58874c43d3962110004ccc
mbalekundri <mbalekundri@gmail.com>
2024-01-18 11:09:44 +0530

    Added src files

---
+++ src/test/java/com/openapi/spring/swagger/SpringBootSwagger3ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapi.spring.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwagger3ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
